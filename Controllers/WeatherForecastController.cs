f6ec96720df8f1c6c9f942a8e6fd365f875a9ad7
afu174 <afu174@gmail.com>
2021-02-07 15:20:21 -0500

    Health

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -29,7 +29,7 @@ namespace Deployment.Controllers
             return Ok();
         }
 
-        [HttpGet()]
+        [HttpGet("Error")]
         public IActionResult NotRunning()
         {
              Response.StatusCode = 400;
